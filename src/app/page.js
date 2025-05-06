a155d91837d764ed8698ba13e916cfa2b56333f2
Anant Padhiyar <133571116+Anant-Padhiyar@users.noreply.github.com>
2025-05-06 18:21:44 +0530

    Now Componets Are in src & Secured Also

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
-import Home from "../../Components/Home";
+import Home from "@/Components/Home";
 
 export default function HomePage() {
   return (
