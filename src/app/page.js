208c590707cceaa73630e5fde9ece4d49f263ff2
Anant Padhiyar <133571116+Anant-Padhiyar@users.noreply.github.com>
2025-05-06 18:16:35 +0530

    Ok Fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,14 @@
 import React from "react";
-
-
 import Image from "next/image";
 import styles from "./page.module.css";
+import Home from "../../Components/Home";
 
-
-export default function Home() {
+export default function HomePage() {
   return (
    
     <>
     <Home/>
-
-
+  
     </>
 
   );
