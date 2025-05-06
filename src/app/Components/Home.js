a61985d74888570aec85a88a21124465c9decc5b
mr-ssc <soyabchhuvara3@gmail.com>
2025-05-06 18:06:50 +0530

    first commit

---
+++ src/app/Components/Home.js
@@ -0,0 +1,13 @@
+// components/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Welcome to TaxiApp</h2>
+      <p>Book your ride quickly and easily.</p>
+    </div>
+  );
+};
+
+export default Home;
